5ff831edeed64a6e27628b391cea0ce3c1411ebf
Nick Zavaritsky <mejedi@gmail.com>
2019-03-24 14:45:43 +0000

    Distinguish pipe and tty in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-systemd-run --user --pipe -E WD=$(pwd) sh -c \
+if [ -t 0 ]; then F=--pty; else F=--pipe; fi
+systemd-run --user $F -E WD=$(pwd) sh -c \
 'CGRP=/sys/fs/cgroup$$(cat /proc/self/cgroup|sed -e s/0:://);\
 mkdir $CGRP/mgmt &&\
 echo $$$$ > $CGRP/mgmt/cgroup.procs &&\
