ab4d80b7643b511849ba1d4fd834a13533a52d5f
Nick Zavaritsky <mejedi@gmail.com>
2019-05-12 15:38:19 +0000

    Auto create missing targets for bind mounts

--- src/mounts.c
+++ src/mounts.c
@@ -1,9 +1,13 @@
+#define _GNU_SOURCE
 #include "sandals.h"
 #include "jshelper.h"
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 static void chroot_relative(
     const char *chroot, const char *path, char buf[PATH_MAX]) {
@@ -17,8 +21,59 @@ static void chroot_relative(
     ) fail(kStatusInternalError, "Path too long");
 }
 
-static void create_dirs(char *path) {
-    // TODO
+// create empty directory or file (depending on src type) to be used as
+// the target of a mount
+static void create_node(const char *src, char *dest)
+{
+    int n = 0, dirfd;
+    struct stat statbuf = { .st_mode = S_IFDIR };
+    char *p;
+
+    if (src && stat(src, &statbuf) == -1)
+        fail(kStatusInternalError, "stat('%s'): %s", src, strerror(errno));
+    p = dest + strlen(dest);
+
+    for (;;) {
+        if (--p <= dest) {
+            dirfd = AT_FDCWD;
+            p = dest;
+            goto create_part;
+        }
+        if (*p == '/') {
+            *p = 0;
+            dirfd = open(dest, O_PATH | O_DIRECTORY | O_CLOEXEC);
+            if (dirfd != -1) break;
+            if (errno != ENOENT)
+                fail(kStatusInternalError,
+                    "open('%s'): %s", dest, strerror(errno));
+            ++n;
+        }
+    }
+
+    do {
+        int fd, flags;
+        *p++ = '/';
+        if (!*p) continue;
+create_part:
+        if (n || (statbuf.st_mode & S_IFMT) == S_IFDIR) {
+            if (mkdirat(dirfd, p, 0700) == -1)
+                fail(kStatusInternalError,
+                    "mkdir('%s'): %s", dest, strerror(errno));
+            if (!n) break;
+            flags = O_PATH | O_DIRECTORY | O_CLOEXEC;
+        } else {
+            flags = O_CREAT | O_WRONLY | O_EXCL | O_CLOEXEC;
+        }
+        fd = openat(dirfd, p, flags, 0600);
+        if (fd == -1)
+            fail(kStatusInternalError,
+                "open('%s'): %s", dest, strerror(errno));
+        p += strlen(p);
+        if (dirfd != AT_FDCWD) close(dirfd);
+        dirfd = fd;
+    } while (n--);
+
+    if (dirfd != AT_FDCWD) close(dirfd);
 }
 
 void do_mounts(const struct sandals_request *request) {
@@ -30,7 +85,8 @@ void do_mounts(const struct sandals_request *request) {
     JSARRAY_FOREACH(request->mounts, mntdef) {
 
         const char *type = NULL, *src = NULL, *dest = NULL, *options = "";
-        int flags = 0, ro = 0, create_missing = 1;
+        int flags = 0;
+        bool ro = false, create_missing = true;
         char chrooted_dest[PATH_MAX];
 
         jsget_object(request->json_root, mntdef);
@@ -83,8 +139,8 @@ retry_mount:
                 fail(kStatusInternalError,
                     "mount('%s','%s','%s'): %s",
                     src, chrooted_dest, type, strerror(errno));
-            create_missing = 0;
-            create_dirs(chrooted_dest);
+            create_missing = false;
+            create_node(flags & MS_BIND ? src : NULL, chrooted_dest);
             goto retry_mount;
         }
 
