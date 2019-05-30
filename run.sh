f107fc039d9be99083351b85d79a068b1195a411
Nick Zavaritsky <mejedi@gmail.com>
2019-05-30 11:05:03 +0000

    Remove samples/ and run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-if [ -t 0 ]; then F=--pty; else F=--pipe; fi
-systemd-run --user $F -E WD=$(pwd) sh -c \
-'CGRP=/sys/fs/cgroup$$(cat /proc/self/cgroup|sed -e s/0:://);\
-mkdir $CGRP/mgmt &&\
-echo $$$$ > $CGRP/mgmt/cgroup.procs &&\
-echo +pids > $CGRP/cgroup.subtree_control &&\
-$WD/sandals'
